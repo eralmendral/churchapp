fb9facca74a7517411bfc556b461fa38cbe3b45f
eralmendral <eralmendral@gmail.com>
2020-07-27 22:27:15 +0800

    display cell leader name in leader column of networks

--- src/app/sharedcomponents/aggrid-renderers-components/agrenderer-cellleader.component.ts
+++ src/app/sharedcomponents/aggrid-renderers-components/agrenderer-cellleader.component.ts
@@ -3,6 +3,9 @@ import { ICellRendererAngularComp }  from 'ag-grid-community/dist/ag-grid-commun
 import { Router,  ActivatedRoute } from '@angular/router';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { DashboardState } from 'src/app/pages/dashboard/reducers';
+import { selectUserById } from 'src/app/pages/dashboard/dashboard.selectors';
 @Component({
   selector: 'app-cellleader-renderer',
   template: '<p>{{ name }}</p>'
@@ -14,13 +17,16 @@ export class CellLeaderRendererComponent implements ICellRendererAngularComp
   public cell: any;
   public users = {};
   public name: string;
-  private profilepic: string;
-  constructor(private afs: AngularFirestore){}
+  public profilepic;
+
+  constructor(private store: Store<DashboardState>){}
   agInit(params: any): void
   {
     this.params = params;
-    this.name = params.value;
-    console.log('debug params:', this.params)
+    this.store.select(selectUserById(params.value)).subscribe(user => {
+      this.name = user.firstname + ' ' + user.lastname;
+    })
+
  }
 
   refresh(): boolean
